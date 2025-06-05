d79d61a20e11254a1160ff0aae2be73086bfa1dd
Ilia <ilia_prog@mail.ru>
2025-06-05 16:02:15 +0300

    add relationship user with email

--- application/core/models/user.py
+++ application/core/models/user.py
@@ -1,10 +1,19 @@
+from core.models.email import Email
 from core.models.mixins.int_id_pk import IntIdPkMixin
 from core.types.user_id import UserIdType
 from .base import Base
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, relationship
+
+from typing import List
 
 
 class User(IntIdPkMixin, Base, SQLAlchemyBaseUserTable[UserIdType]):
+
+    sent_emails: Mapped[List["Email"]] = relationship(
+        back_populates="sender",
+        cascade="all, delete-orphan",
+    )
 
     @classmethod
     def get_db(cls, session):
